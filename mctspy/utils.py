c904714774b2dae14c7ada2ae80dd6bc8ba69326
ttong-ai <ttong@pro-ai.org>
2024-09-19 19:45:58 -0700

    Init utils.py

---
+++ mctspy/utils.py
@@ -0,0 +1,25 @@
+# utils.py
+import copyreg
+import types
+
+def _pickle_method(method):
+    func_name = method.__func__.__name__
+    obj = method.__self__
+    cls = method.__class__
+    return _unpickle_method, (func_name, obj, cls)
+
+def _unpickle_method(func_name, obj, cls):
+    func = None
+    for cls in cls.mro():
+        try:
+            func = cls.__dict__[func_name]
+        except KeyError:
+            pass
+        else:
+            break
+    if func is None:
+        raise AttributeError(f"Method {func_name} not found")
+    return func.__get__(obj, cls)
+
+# Register the pickle functions
+copyreg.pickle(types.MethodType, _pickle_method, _unpickle_method)
